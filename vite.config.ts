01b15d565e20a869d2859e3ff45f923883054960
Lars Nieradzik <l.nieradzik@gmail.com>
2024-08-11 02:39:15 +0200

    wrong path

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/dontknow103.github.io/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
 })
