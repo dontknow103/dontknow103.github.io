474413e6b1e52e0420eb321b9f6ccf19a22f8c99
Lars Nieradzik <l.nieradzik@gmail.com>
2024-08-16 20:21:40 +0200

    forgot to remove

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
-import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    crossOriginIsolation(),
     viteStaticCopy({
       targets: [
         {
